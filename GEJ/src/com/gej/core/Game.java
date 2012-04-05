7579ed77c499db1a2836f8ca67e56ffeec409467
sriharshachilakapati <sriharshachilakapati@gmail.com>
2012-04-05 07:14:32 +0530

    Update

--- GEJ/src/com/gej/core/Game.java
+++ GEJ/src/com/gej/core/Game.java
@@ -1,6 +1,19 @@
 package com.gej.core;
 
-public class Game {
+import javax.swing.JPanel;
+
+/**
+ * This class is the main class for any game. You should extend this class
+ * to write a game.
+ * 
+ * @author Sri Harsha Chilakapati
+ */
+public class Game extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5934394613281562786L;
 
 	
 	
