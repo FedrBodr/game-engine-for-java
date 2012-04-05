f92f27e4ae8d3dbe2017b586b3c7379fe2e867aa
sriharshachilakapati <sriharshachilakapati@gmail.com>
2012-04-05 07:23:30 +0530

    Update

---
+++ GEJ/src/com/gej/core/Global.java
@@ -0,0 +1,15 @@
+package com.gej.core;
+
+/**
+ * This class contains static fields which can be called by any
+ * sprite, or the game class itself.
+ * 
+ * @author Sri Harsha Chilakapati
+ */
+public class Global {
+	
+	public static String TITLE  = "GEJ - The simplest GameEngine for Java";
+	public static int    WIDTH  = 640;
+	public static int    HEIGHT = 480;
+
+}
