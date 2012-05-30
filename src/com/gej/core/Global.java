f1512464f2e6871e2b669ac1bb9882f21418c47d
sriharshachilakapati <sriharshachilakapati@gmail.com>
2012-05-30 12:25:20 +0530

    Update

--- src/com/gej/core/Global.java
+++ src/com/gej/core/Global.java
@@ -27,6 +27,6 @@ public class Global {
 	/** A map containing various properties */
 	public static HashMap<String, Object> PROPERTIES = new HashMap<String, Object>();
 	/** Variable containing the current GEJ version */
-	public static String GEJ_VERSION                 = "GEJ V0.4 R3";
+	public static String GEJ_VERSION                 = "GEJ V0.5";
 	
 }
